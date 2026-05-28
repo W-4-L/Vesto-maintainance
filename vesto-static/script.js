5e000ba3d3ca27ed460259ffe4e33053038345e0
W-4-L <146171461+W-4-L@users.noreply.github.com>
2026-05-28 17:06:44 +0530

    Delete vesto-static/script.js

--- vesto-static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var main = document.getElementById('main');
-  var footer = document.getElementById('footer');
-
-  setTimeout(function () {
-    main.classList.add('visible');
-    footer.classList.add('visible');
-  }, 80);
-});
